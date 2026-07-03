05b3d5bc0664c0a7f068238517f768a989e86d3a
Patrickess <66pat66@seznam.cz>
2026-07-03 15:34:58 +0200

    Add files via upload
    haha

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+let canOpenLetter = false;
 
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.querySelector(".wrapper").classList.add("open");
+        canOpenLetter = true;
+    }, 1500);
+});
+
+const wrapper = document.querySelector(".wrapper");
+const audio = document.querySelector("#bgMusic");
+
+wrapper.addEventListener("click", () => {
+
+    if (!canOpenLetter) {
+        return;
+    }
+
+    wrapper.classList.add("expanded");
+
+    audio.currentTime = 0;
+    audio.volume = 0.6;
+	console.log(audio);
+
+    audio.play().catch(err => {
+        console.log("Audio blocked:", err);
+    });
+});
